f5e5252985707f95aad6e6a9346c3e54da24bca3
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-21 22:49:58 +0100

    init login component

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,8 @@
+export function LogIn(){
+    return (
+        <div>
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Log In</h3>
+        <p>Page is not completed yet</p>
+        </div>
+    )
+}
