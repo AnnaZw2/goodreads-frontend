fdcb5b4a4dbd6d9f5f060995ddb819948614b005
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-21 23:20:15 +0100

    created route to myprofile

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { Home } from './pages/Home';
 import { SignIn } from './pages/SignIn';
 import { LogIn } from './pages/LogIn';
+import { MyProfile } from './pages/MyProfile';
 
 
 function App() {
@@ -18,12 +19,22 @@ function App() {
     <Link to="/" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Home</Link>
     <Link  to="/sign" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Sign in</Link>
     <Link  to="/login" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Log in</Link>
+    
     </div>
+
+    {/* CHANGE ALL OF IT TO DROPDOWN MENU */}
+    <div className='flex justify-end ' >
+    {/* in settings there will be option to change name/username/email/password, delete your account, import to file your account info, maybe to add some description to your profile */}
+      <Link to="/settings" className=' text-light-brown border border-brown w-fit rounded-lg p-1 hover:bg-yellow hover:text-light-brown'>Settings</Link>
+      
+    </div>
+  
       <Routes>
       
         <Route exact path="/" element={<Home/>}></Route>
         <Route exact path='/sign' element={<SignIn/>}></Route>
         <Route exact path='/login' element={<LogIn/>}></Route>
+        <Route exact path='/settings' element={<MyProfile/>}></Route>
       </Routes>
  
     </div>
---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,8 @@
+export function MyProfile(){
+    return (
+        <div>
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600">Settings</h3>
+        <p>Page is not completed yet</p>
+        </div>
+    )
+}
