fb90922085676645c2bf6023477f81d25552b39b
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-21 22:47:09 +0100

    Added new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,25 @@ import { Link } from 'react-router-dom';
 import './App.css'
 import { Home } from './pages/Home';
 import { SignIn } from './pages/SignIn';
+import { LogIn } from './pages/LogIn';
 
 
 function App() {
 
 
   return (
-    <div className="App " >
+    <div className="root bg-beige min-h-screen " >
 
-    <Link to="/" >Home</Link>
-    <Link  to="/sign" >Sign in</Link>
+    <div className='flex justify-center gap-2 font-semibold text-xl '>
+    <Link to="/" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Home</Link>
+    <Link  to="/sign" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Sign in</Link>
+    <Link  to="/login" className=' text-light-brown border border-brown rounded-lg p-1 hover:bg-yellow hover:text-light-brown' >Log in</Link>
+    </div>
       <Routes>
       
-        <Route exact path="/" element={<Home/>}>
-        </Route>
+        <Route exact path="/" element={<Home/>}></Route>
         <Route exact path='/sign' element={<SignIn/>}></Route>
+        <Route exact path='/login' element={<LogIn/>}></Route>
       </Routes>
  
     </div>
