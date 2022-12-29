6c9315e8ca0d523a6d9a6d2cf4434235fbb87342
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-29 18:07:25 +0100

    added error msg when shelve name is not unique

--- app/src/components/books/AddNewShelf.jsx/AddClicked.jsx
+++ app/src/components/books/AddNewShelf.jsx/AddClicked.jsx
@@ -3,61 +3,64 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export function AddClicked({ setAdding, shelves }) {
-  const [inputValue, setInputValue] = useState("");
-  const [CanClick, setCanClick] = useState(false);
-
-  useEffect(() => {
-    const nameIsUnique = shelves.filter(el => el.name==inputValue).length==0
-    console.log("unique: " +nameIsUnique)
-    console.log("change input value");
-    if (inputValue.length != 0 && nameIsUnique ) {
-      setCanClick(true);
-    } else {
-      setCanClick(false);
-    }
-  }, [inputValue]);
-
-  function handleClick() {
-    if(CanClick){
-        const newSortValue = shelves[shelves.length - 1].sort + 10;
-    console.log(shelves[shelves.length - 1].sort);
-    axios
-      .post(
-        "http://localhost:3000/shelves",
-        { sort: newSortValue, name: inputValue },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
+    const [inputValue, setInputValue] = useState("");
+    const [CanClick, setCanClick] = useState(false);
+    const [error, setError] = useState("");
+    const nameIsUnique =
+        shelves.filter((el) => el.name == inputValue).length == 0;
+    useEffect(() => {
+        console.log("unique: " + nameIsUnique);
+        console.log("change input value");
+        if (inputValue.length != 0 && nameIsUnique) {
+            setError("")
+            setCanClick(true);
+        } else {
+            if (!nameIsUnique) {
+                setError("Shelve already exists!");
+            }
+            setCanClick(false);
         }
-      )
-      .then(res => {if(res.status==200){
-        setAdding(false)
-      }})
-      .catch((err) => console.log(err));
+    }, [inputValue]);
 
-
+    function handleClick() {
+        if (CanClick) {
+            const newSortValue = shelves[shelves.length - 1].sort + 10;
+            console.log(shelves[shelves.length - 1].sort);
+            axios
+                .post(
+                    "http://localhost:3000/shelves",
+                    { sort: newSortValue, name: inputValue },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                )
+                .then((res) => {
+                    if (res.status == 200) {
+                        setAdding(false);
+                    }
+                })
+                .catch((err) => console.log(err));
+        }
     }
-    
-   
-  }
 
-  return (
-    <div className="btn-style  border-t pb-1">
-      <input
-        type="text"
-        placeholder="New shelf name"
-        className="border m-3"
-        onChange={(e) => setInputValue(e.target.value)}
-      ></input>
-      <button
-        className={` ${
-          CanClick ? `hover text-black ` : `text-light-gray cursor-auto`
-        } bg-light-beige px-2 border rounded-sm`}
-        onClick={handleClick}
-      >
-        Add
-      </button>
-    </div>
-  );
+    return (
+        <div className="btn-style  border-t pb-1">
+            <input
+                type="text"
+                placeholder="New shelf name"
+                className="border m-3"
+                onChange={(e) => setInputValue(e.target.value)}
+            ></input>
+            {error.length != 0 ? <p className="text-red">{error}</p> : null}
+            <button
+                className={` ${CanClick ? `hover text-black ` : `text-light-gray cursor-auto`
+                    } bg-light-beige px-2 border rounded-sm`}
+                onClick={handleClick}
+            >
+                Add
+            </button>
+        </div>
+    );
 }
