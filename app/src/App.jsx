57576ba64fbc7a2e42ac9b5c9cd4c0c4f180d4d0
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-29 18:21:59 +0100

    fixed background color displaying not on all background

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
 
   return (
-    <div className="root bg-light-beige min-h-screen " >
+    <div className="root bg-light-beige max-h-screen " >
     <Navbar/> 
       <Routes>
 
