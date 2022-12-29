5b5c25caeee0a022a21b862f7b1ff1e02fa513f0
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2022-12-29 18:26:57 +0100

    fixed background

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
 
   return (
-    <div className="root bg-light-beige max-h-screen " >
+    <div className="root bg-light-beige min-h-screen " >
     <Navbar/> 
       <Routes>
 
