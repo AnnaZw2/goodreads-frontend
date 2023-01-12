ad5e9f261932216613ceb70efb64141ac04eb4c9
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-12 15:22:06 +0100

    checking for jwt token

--- app/src/components/books/displayBooks.jsx
+++ app/src/components/books/displayBooks.jsx
@@ -1,28 +1,27 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
 import { Book } from "./book";
-
 
 export  function DisplayBooks(){
     const [books, setBooks] = useState([]);
+
+    const jwt = localStorage.getItem("jwt")
   
 
 useEffect(() => {
   console.log("display books")
   axios
-    .get("http://localhost:3000/books")
+    .get("http://localhost:3000/books",{headers:{"Authorization":`Bearer ${jwt}`}})
     .then((res) => {
 
       setBooks(res.data);
      
     })
 
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err) );
 },[]);
 return(
     <div>
-
-     {/* <ul> {books.map(el=><li key={el.id}><img className="h-32 w-auto" src={el.cover} ></img></li>)}</ul> */}
      <ul>{books.map((el)=><li key={el._id} ><Book title={el.title} cover={el.cover} description={el.description} author={el.author}/></li>)}</ul>
    
      </div>
