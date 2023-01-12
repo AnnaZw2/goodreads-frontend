49fb9443c9136776faaffc96fc1d990a402f3fe0
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-12 15:19:18 +0100

    added navbar and jwt token

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { DisplayBooks } from "../components/books/displayBooks";
+import { Navbar } from "../components/navbar";
+
 export function Home() {
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    { localStorage.getItem("jwt") == null ? navigate("/login") : null }
+  }, [])
+
 
   return (
     <div className="min-w-screen">
-     <DisplayBooks/>
+      <Navbar />
+      <DisplayBooks />
     </div>
   );
 }
