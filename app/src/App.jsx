229c334b9ee52ec993a95fed61b36a7db993564b
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-12 15:22:49 +0100

    update imports after renaming file

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,10 @@
 
-import React from 'react';
-import { Route, Routes } from 'react-router';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router';
 import './App.css'
 import { Home } from './pages/Home';
 import { Explore } from './pages/Explore';
-import { SignIn } from './pages/BeforeLogIn/SignIn';
+import { Register } from './pages/BeforeLogIn/Register';
 import { LogIn } from './pages/BeforeLogIn/LogIn';
 import { NotLoggedIn } from "./pages/BeforeLogIn/NotLoggedIn"
 import { Settings } from './pages/Settings';
@@ -18,27 +18,27 @@ import { Navbar } from './components/navbar';
 
 function App() {
 
-
   return (
-    <div className="root bg-light-beige min-h-screen " >
-    <Navbar/> 
-      <Routes>
+      <div className="root bg-light-beige min-h-screen " >
 
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path='/sign' element={<SignIn />}></Route>
-        <Route exact path='/login' element={<LogIn />}></Route>
-        <Route exact path='/explore' element={<Explore />}></Route>
-        <Route exact path='/settings' element={<Settings />}></Route>
-        <Route exact path="/notlogged" element={<NotLoggedIn />}></Route>
-        <Route exact path='/mybooks' element={<MyBooks />}>    </Route>
-        <Route exact path='mybooks/shelves/all' element={<All/>}></Route>
-        <Route exact path='mybooks/shelves/currentlyreading' element={<CurrentlyReading/>}></Route>
-        <Route exact path='mybooks/shelves/read' element={<Read/>}></Route>
-        <Route exact path='mybooks/stats' element={<Stats/>}></Route>
 
-      </Routes>
+        <Routes>
 
-    </div>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path='/sign' element={<Register />}></Route>
+          <Route exact path='/login' element={<LogIn />}></Route>
+          <Route exact path='/explore' element={<Explore />}></Route>
+          <Route exact path='/settings' element={<Settings />}></Route>
+          <Route exact path="/notlogged" element={<NotLoggedIn />}></Route>
+          <Route exact path='/mybooks' element={<MyBooks />}>    </Route>
+          <Route exact path='mybooks/shelves/all' element={<All />}></Route>
+          <Route exact path='mybooks/shelves/currentlyreading' element={<CurrentlyReading />}></Route>
+          <Route exact path='mybooks/shelves/read' element={<Read />}></Route>
+          <Route exact path='mybooks/stats' element={<Stats />}></Route>
+
+        </Routes>
+
+      </div>
   )
 }
 
