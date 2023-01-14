7028f5a73eb567fe8f32f4317d7dca68bcc227de
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-14 15:07:24 +0100

    refactoring with code formater

--- app/src/pages/MyBooks/navigateMyBooks.jsx
+++ app/src/pages/MyBooks/navigateMyBooks.jsx
@@ -2,25 +2,25 @@ import { json, Link, useParams } from "react-router-dom"
 import "./NavigateMyBooks.css"
 import axios from "axios";
 import { useContext, useEffect, useMemo, useState } from "react";
-import { AddClicked } from "../../components/books/AddNewShelf.jsx/AddClicked";
+import { AddClicked } from "../../components/books/AddNewShelf/AddClicked";
 import { userContext } from "../../context/userContex";
 
 export function NavigateMyBooks() {
-    const {jwt} = useContext(userContext)
+    const { jwt } = useContext(userContext)
 
-    const [shelves,setShelves]= useState([])
+    const [shelves, setShelves] = useState([])
 
     const arr = []
     useEffect(() => {
         console.log(shelves)
-      axios.get("http://localhost:3000/shelves", { headers: { "Authorization": `Bearer ${jwt}` } })
+        axios.get("http://localhost:3000/shelves", { headers: { "Authorization": `Bearer ${jwt}` } })
             .then(res => {
                 res.data.map(el => arr.push(el.name))
                 setShelves(arr)
-                
+
             })
-            .catch(err=> console.log(err))
-      
+            .catch(err => console.log(err))
+
     }, [shelves])
 
     return (
@@ -31,9 +31,9 @@ export function NavigateMyBooks() {
                 {/* shelves */}
                 <div className="section [&>*:nth-child(5)]:border-b border-brown">
                     <p>Bookshelves</p>
-                
-                  <li className={"list-none "}> <Link to="/mybooks/shelves/all" className="links">All</Link></li> 
-                    {shelves.map(el =><li  className={"list-none"}> <Link to={`/mybooks/shelves/${el.toLowerCase().replace(/\s+/g, '')}`} className="links">{el}</Link></li>)}
+
+                    <li className={"list-none "}> <Link to="/mybooks/shelves/all" className="links">All</Link></li>
+                    {shelves.map(el => <li className={"list-none"}> <Link to={`/mybooks/shelves/${el.toLowerCase().replace(/\s+/g, '')}`} className="links">{el}</Link></li>)}
 
                 </div>
 
@@ -42,7 +42,7 @@ export function NavigateMyBooks() {
                     <Link to="/mybooks/stats" className="links">Stats</Link>
                 </div>
 
-              
+
             </div>
         </div>
     )
