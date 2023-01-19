d6fb33a6698767e3dc12622a9df136a2066b5209
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-19 23:09:47 +0100

    formated with prettier

--- app/src/components/books/book.jsx
+++ app/src/components/books/book.jsx
@@ -1,17 +1,15 @@
 import { AddToShelf } from "../../pages/Home/addToShelf";
+
 export function Book({ title, cover, description, author }) {
-
-    return (
-        <div className="flex m-48 p-8 border rounded-xl bg-white">
-            <img src={cover} className="h-74 w-26" />
-
-            <div className="flex flex-col ml-5 ">
-                <h3 className="header-3">{title}</h3>
-                <p className="text-lg mb-6">by {author}</p>
-                <AddToShelf />
-                {/* <button className="bg-green text-white cursor-pointer border rounded-md w-48 self-center mb-12 p-3" >Add to shelf</button> */}
-                <div>{description}</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex mb-40  ml-48 mr-48 p-8 border rounded-xl bg-white">
+      <img src={cover} className="h-74 w-26" />
+      <div className="flex flex-col ml-5 ">
+        <h3 className="header-3">{title}</h3>
+        <p className="text-lg mb-6">by {author}</p>
+        <AddToShelf />
+        <div>{description}</div>
+      </div>
+    </div>
+  );
 }
