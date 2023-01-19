66f1146944976aeb1c505dd4071624650ec7f547
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-19 23:10:23 +0100

    deleted unnescesary code

--- app/src/components/deleteButton.jsx
+++ app/src/components/deleteButton.jsx
@@ -1,25 +1,13 @@
 import axios from "axios";
-import { useContext, useEffect, useRef } from "react";
+import { useContext} from "react";
 import { updateShelfContext } from "../context/updateShelfContext";
 import { useState } from "react";
 import { Modal } from "./Modal";
-import useOutsideClick from "../hooks/useClickOutside"
+
 
 export function DeleteButton({ request, textButton, textModal }) {
     const { updateShelves, setUpdateShelves } = useContext(updateShelfContext);
     const [openConfirm, setOpenConfirm] = useState(false);
-
-
-
-    const modalRef = useRef(null);
-    const buttonRef = useRef(null);
-const mycallback = () => {
-    console.log("yuuuup")
-        setOpenConfirm(false)
-
-}
-
-    useOutsideClick(modalRef, buttonRef,mycallback);
 
 
     const handleDelete = () => {
@@ -37,7 +25,7 @@ const mycallback = () => {
     return (
         <div>
             <button
-                ref={buttonRef}
+                // ref={buttonRef}
                 onClick={() => {
                     handleDelete();
                 }}
@@ -50,7 +38,7 @@ const mycallback = () => {
                 {openConfirm ? (
 
                     <Modal
-                        modalRef={modalRef}
+                        // modalRef={modalRef}
                         textModal={textModal}
                         setOpenConfirm={setOpenConfirm}
                         handleConfirm={handleConfirm}
