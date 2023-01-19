71738d5a4aee3e12446647cfb3f4a11286be656d
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-19 23:10:09 +0100

    added useClose hook

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,15 +1,19 @@
+import { useRef } from "react";
+import { useClose } from "../hooks/useClose";
+
 import "./../index.css";
 import "./Modal.css"
 
-export function Modal({ textModal, setOpenConfirm, handleConfirm ,modalRef}) {
-
+export function Modal({ textModal, setOpenConfirm, handleConfirm }) {
+const ref = useRef()
+useClose(ref,()=>{setOpenConfirm(false)})
   return (
     <div className="modal-background fixed top-0 left-0 w-full h-full flex items-center justify-center z-10">
-     <div ref={modalRef} className="modal-content relative  bg-white border rounded-lg p-6 z-20">
+     <div ref={ref} className="modal-content relative  bg-white border rounded-lg p-6 z-20">
    
         <div className="absolute top-0 right-0 m-3">
-          <button className="text-blackborder  rounded-sm p-2 hover:bg-light-brown font-medium hover:text-black focus:outline-none focus:text-black">
-            <p onClick={() =>{ setOpenConfirm(false)}}>X</p>
+          <button className="text-black  flex justify-center items-center rounded-sm hover:bg-light-gray w-8 h-8 font-medium hover:text-black focus:outline-none focus:text-black" onClick={() =>{ setOpenConfirm(false)}}>
+            X
           </button>
         </div>
         <div className="text-lg font-medium mb-4">
