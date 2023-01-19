b54b53b2636fd5aefafd52b7629b7d53e0fb7882
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-19 18:52:59 +0100

    formated file with prettier

--- app/src/components/deleteButton.jsx
+++ app/src/components/deleteButton.jsx
@@ -1,43 +1,41 @@
-import axios from "axios"
-import { useContext, useEffect } from "react"
-import { updateShelfContext } from "../context/updateShelfContext"
-import { userContext } from "../context/userContex"
-import { useState } from "react"
-import { Modal } from "./Modal"
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { updateShelfContext } from "../context/updateShelfContext";
+import { userContext } from "../context/userContex";
+import { useState } from "react";
+import { Modal } from "./Modal";
 
 export function DeleteButton({ request, textButton, textModal }) {
-    const { jwt } = useContext(userContext)
-    const { updateShelves, setUpdateShelves } = useContext(updateShelfContext)
-    const [confirm, setConfirm] = useState(null)
-    const [openConfirm, setOpenConfirm] = useState(false)
-    console.log(updateShelves, "shelves")
-    const handleDelete = () => {
-        setOpenConfirm(true)
-    }
-    const handleConfirm = () => {
-        request()
+  const { updateShelves, setUpdateShelves } = useContext(updateShelfContext);
+  const [openConfirm, setOpenConfirm] = useState(false);
 
-    }
-    return (
-        <div>
-            <button onClick={() => { handleDelete() }} className="text-xs text-red">{textButton}</button>
+  const handleDelete = () => {
+    setOpenConfirm(true);
+  };
+  const handleConfirm = () => {
+    request();
+    setUpdateShelves(true);
+  };
+  return (
+    <div>
+      <button
+        onClick={() => {
+          handleDelete();
+        }}
+        className="text-xs text-red"
+      >
+        {textButton}
+      </button>
 
-            <div className="relative">
-
-
-                {/* {openConfirm ? */}
-                {/* <div className="bg-white w-32 absolute border ">
-         <p>Are you sure you want to delete this shelf?</p>
-
-          <div className="flex flex-row gap-2 justify-center ">
-          <button onClick={()=>{handleConfirm(id); setOpenConfirm(false),setUpdateShelves(true)}}><p className="text-green">Yes</p></button>
-         <button onClick={()=>{setConfirm(false),setOpenConfirm(false)}}><p className="text-red">No</p></button>
-         </div>
-
-         </div> */}
-                {openConfirm ? <Modal textModal={textModal} /> : null}
-
-            </div>
-        </div>
-    )
+      <div className="relative">
+        {openConfirm ? (
+          <Modal
+            textModal={textModal}
+            setOpenConfirm={setOpenConfirm}
+            handleConfirm={handleConfirm}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 }
