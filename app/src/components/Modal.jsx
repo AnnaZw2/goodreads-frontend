12fcb9d0491cfe7524addce7e77512139fd289bf
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-19 18:52:19 +0100

    added background to whole page

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,25 +1,42 @@
-import "./../index.css"
-export function Modal({textModal}){
-    return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-        <div className="relative bg-white rounded-lg p-6">
-          <div className="absolute top-0 right-0">
-            <button className="text-black font-medium hover:text-black focus:outline-none focus:text-black">
-              X
-            </button>
-          </div>
-          <div className="text-lg font-medium mb-4 "><h4 className="header-4">Confirm</h4></div>
-          <div className="text-gray-700 mb-4">{textModal}</div>
-          <div className="flex justify-center">
-            <button className="text-gray-700 font-medium  hover:text-red  focus:outline-none focus:text-gray-900 mr-2">
-              Cancel
-            </button>
-            <button className="bg-indigo-500  font-medium  hover:text-green p-3 focus:outline-none focus:bg-indigo-600">
-              Continue
-            </button>
-          </div>
+import "./../index.css";
+import "./Modal.css"
+
+
+export function Modal({ textModal, setOpenConfirm, handleConfirm }) {
+
+
+
+  return (
+    <div className="modal-background fixed top-0 left-0 w-full h-full flex items-center justify-center z-10">
+     <div  className="modal-content relative  bg-white border rounded-lg p-6 z-20">
+   
+        <div className="absolute top-0 right-0 m-3">
+          <button className="text-blackborder  rounded-sm p-2 hover:bg-light-brown font-medium hover:text-black focus:outline-none focus:text-black">
+            <p onClick={() =>{ setOpenConfirm(false)}}>X</p>
+          </button>
+        </div>
+        <div className="text-lg font-medium mb-4">
+          <h4 className="header-4">Confirm</h4>
+        </div>
+        <div className="text-gray-700 mb-4">{textModal}</div>
+        <div className="flex gap-3 justify-center">
+          <button
+            className="bg-transparent hover:bg-red text-red font-semibold hover:text-white py-2 px-4 border border-red hover:border-transparent rounded"
+            onClick={() => {setOpenConfirm(false)}}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-transparent  hover:bg-green text-green font-semibold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded"
+            onClick={() => {
+              setOpenConfirm(false);
+              handleConfirm();
+            }}
+          >
+            Continue
+          </button>
         </div>
       </div>
-      
-    )
+    </div>
+  );
 }
