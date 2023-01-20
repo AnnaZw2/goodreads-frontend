f6d70136dc502d83d14db84404c842979cdd8571
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-20 22:58:33 +0100

    added admin button

--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -1,56 +1,80 @@
+import { useContext,useEffect,useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { userContext } from "../context/userContex";
 
 
 export function Navbar() {
+  const navigate = useNavigate();
+
+  const {user,setUpdateUser} = useContext(userContext)
+  
 
 
-    const navigate = useNavigate()
-    return (
-
-        <nav className="bg-beige flex flex-row align-top justify-between">
-            <div className="flex  flex-row ">
-                <Link
-                    to="/"
-                    className="flex  justify-start text-black  p-3  hover:bg-black hover:text-white w-fit"
-                >
-                    Home
-                </Link>
-                <Link
-                    to="/mybooks"
-                    className="flex justify-center text-black p-3  w-fit hover:bg-black hover:text-white"
-                >
-                    My Books
-                </Link>
-
-                <Link
-                    to="/explore"
-                    className="flex justify-center text-black p-3  w-fit hover:bg-black hover:text-white"
-                >
-                    Explore
-                </Link>
-
-            </div>
-            <div className="flex flex-row align-center    self-center">
-
-                <input type="search" placeholder="Search books" className="flex  sm:h-9  p-4  rounded-md w-80 border border-brown 
-"></input>
-            </div>
 
 
-            {/* CHANGE ALL OF IT TO DROPDOWN MENU */}
-            <div className="flex flex-row  ">
-                {/* in settings there will be option to change name/username/email/password, delete your account, import to file your account info, maybe to add some description to your profile */}
-                <Link
-                    to="/settings"
-                    className=" text-black   w-fit p-3 hover:bg-black hover:text-beige"
-                >
-                    Settings
-                </Link>
+  return (
+    <nav className="bg-beige flex flex-row align-top justify-between">
+      <div className="flex  flex-row ">
+        <Link
+          to="/"
+          className="flex  justify-start text-black  p-3  hover:bg-black hover:text-white w-fit"
+        >
+          Home
+        </Link>
+        <Link
+          to="/mybooks"
+          className="flex justify-center text-black p-3  w-fit hover:bg-black hover:text-white"
+        >
+          My Books
+   
+        </Link>
+     
 
-                {/* TO be added feature that signs out and ridarects to log in page */}
-                <button className=" hover:bg-black p-3 hover:text-white" onClick={() => { localStorage.removeItem("jwt"); navigate("/login") }}>Sign Out</button>
-            </div>
-        </nav>
+        <Link
+          to="/explore"
+          className="flex justify-center text-black p-3  w-fit hover:bg-black hover:text-white"
+        >
+          Explore
+        </Link>
+      </div>
+      <div className="flex flex-row align-center    self-center">
+        <input
+          type="search"
+          placeholder="Search books"
+          className="flex  sm:h-9  p-4  rounded-md w-80 border border-brown 
+"
+        ></input>
+      </div>
 
-    );
+      <div className="flex flex-row  ">
+      {user.role=="admin" ?
+        <Link
+          to="/admin"
+          className=" text-black   w-fit p-3 hover:bg-black hover:text-beige"
+        >
+          Admin
+        </Link> : null }
+
+    
+        <Link
+          to="/users"
+          className=" text-black   w-fit p-3 hover:bg-black hover:text-beige"
+        >
+          My Profile
+        </Link>
+
+        <button
+          className=" hover:bg-black p-3 hover:text-white"
+          onClick={() => {
+            localStorage.removeItem("jwt");
+            localStorage.removeItem("decoded")
+            // setUpdateUser(null)
+            navigate("/login");
+          }}
+        >
+          Sign Out
+        </button>
+      </div>
+    </nav>
+  );
 }
