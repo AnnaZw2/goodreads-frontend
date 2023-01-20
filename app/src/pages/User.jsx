be26d313b6b4f78e7c46e4af55ac317161f59ecf
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-20 23:00:22 +0100

    aded my profile page

--- app/src/pages/User.jsx
+++ app/src/pages/User.jsx
@@ -1,18 +1,26 @@
 import { Navbar } from "../components/navbar";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { userContext } from "./../context/userContex";
 import { DeleteButton } from "../components/deleteButton";
 import axios from 'axios'
+import { UpdateButton } from "../components/UpdateButton";
 function User(){
     const {user,jwt} = useContext(userContext)
 
-  
     return(
         <div>
         <Navbar/>
+       
         <h4 className="header-4 p-3">My Profile</h4>
-        <p>Email: {user.email}</p>
-        <DeleteButton text={"Delete account"} request={axios.delete(`http://localhost:3000/users/{${user.email}}`,{ headers: { "Authorization": `Bearer ${jwt}` }}).catch(err => console.log(err))}/>
+       
+         <div>
+        <p><strong>Email: </strong>{user.email}</p>
+        <div className="flex flex-row justify-center gap-1">
+        <p><strong>Username:</strong> {user.username}</p>
+        <UpdateButton patchKey={"username"} textButton={"Edit"} textModal={<p>Are you sure you want to change this username?</p>} placeholder="Enter new username" url={`http://localhost:3000/users/${user.email}`}/>
+        </div>
+       
+        <DeleteButton textModal={"Are you sure you want to delet this account?"} textButton={"Delete account"} request={()=>axios.delete(`http://localhost:3000/users/{${user.email}}`,{ headers: { "Authorization": `Bearer ${jwt}` } }).then(res => console.log(res)).catch(err => console.log(err))}/></div> 
         </div>
     )
 
