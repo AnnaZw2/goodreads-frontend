e4376f35b46dc309d111e124296e2d532b3985aa
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-21 12:37:24 +0100

    linked cover to details

--- app/src/pages/Explore.jsx
+++ app/src/pages/Explore.jsx
@@ -1,8 +1,7 @@
 import { Navbar } from "../components/navbar";
-import { useLocation } from 'react-router-dom'
-import { useContext, useEffect } from "react";
-import { DisplayBooks } from "../components/books/displayBooks";
+import { useContext} from "react";
 import { searchShelfContext } from "../context/searchContext";
+import { Link } from "react-router-dom";
 export function Explore() {
  
     const {searchValue,searchOutput} = useContext(searchShelfContext)
@@ -10,14 +9,18 @@ export function Explore() {
     return (
         <div>
         <Navbar/>
-        <div className="flex flex-wrap">
-  {searchOutput.length !== 0 ? searchOutput.map((el) => (
-    <div className="w-1/4 p-4">
-      
-      <img className="w-full" src={el.cover} alt={el.title} />
-    </div>
-  )) : null}
+        <div className="mr-24 ml-24 mt-10">
+  <div className="flex flex-wrap">
+    {searchOutput.length !== 0 ? searchOutput.map((el) => (
+      <div className="w-1/4 p-4">
+      <Link className="links" to={`/details/${el._id}`} >
+        <img className="w-full  " src={el.cover} alt={el.title} />
+        </Link>
+      </div>
+    )) : null}
+  </div>
 </div>
+
 
             <h3 className="header-3">Explore</h3>
             <p>Page is not completed yet</p>
