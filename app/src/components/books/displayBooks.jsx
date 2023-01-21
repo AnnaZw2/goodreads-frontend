6a8caa4cbf0c0e7be69ab8ade9aa8f7f90fa10b9
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-21 18:35:18 +0100

    deleted commented code

--- app/src/components/books/displayBooks.jsx
+++ app/src/components/books/displayBooks.jsx
@@ -1,27 +1,8 @@
-import axios from "axios";
-import { useContext, useEffect, useState} from "react";
-import { userContext } from "../../context/userContex";
 import { Book } from "./book";
 
 export  function DisplayBooks({books}){
-    // const [books, setBooks] = useState([]);
 
 
-    
-
-
-// useEffect(() => {
-//   console.log("display books")
-//   axios
-//     .get("http://localhost:3000/books",{headers:{"Authorization":`Bearer ${jwt}`}})
-//     .then((res) => {
-
-//       setBooks(res.data);
-     
-//     })
-
-//     .catch((err) => console.log(err) );
-// },[]);
 return(
     <div >
      <ul className="[&>*:nth-child(1)] mt-20">{books.map((el)=><li key={el._id} ><Book title={el.title} id={el._id} cover={el.cover} description={el.description} author={el.author} style={{imgHeight:"h-74"}}/></li>)}</ul>
