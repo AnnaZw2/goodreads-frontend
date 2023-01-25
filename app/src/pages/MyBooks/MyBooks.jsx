e0268e2785a3c0b5d8ca9ade7a3d9c59dbe6a85f
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-26 00:18:52 +0100

    refactored

--- app/src/pages/MyBooks/MyBooks.jsx
+++ app/src/pages/MyBooks/MyBooks.jsx
@@ -1,20 +1,69 @@
 
 import { Navbar } from "../../components/navbar"
 import { NavigateMyBooks } from "./navigateMyBooks"
-export function MyBooks({shelfName,books}) {
+import "./MyBooks.css"
+import axios from "axios"
+import { useContext } from "react"
+import { userContext } from "../../context/userContex"
+export function MyBooks({shelfName,books,setBooks}) {
+const {jwt} =useContext(userContext)
+
+    // function handleClick(bookId){
+    //  const arr =books.filter(el => el._id!=bookId)
+    // //  setBooks(arr)
+    // //  axios.patch()
+    // axios
+    // .get(`http://localhost:3000/book-details?book_id=${bookId}`, {
+    //   headers: { Authorization: `Bearer ${jwt}` },
+    // })
+    // .then((res) => {
+           
+    //       axios
+    //         .patch(
+    //           `http://localhost:3000/book-details/${res.data[0]._id}`,
+    //           {
+    //             shelves: selectedStandardShelves.concat(
+    //               getArrayFromSelectedCustomShelves(customChecked)
+    //             ),
+    //           },
+    //           {
+    //             headers: {
+    //               Authorization: `Bearer ${jwt}`,
+    //               "Content-type": "application/json",
+    //             },
+    //           }
+    //         )
+    //           .then(()=> setUpdateShelves(true))
+    //         .catch((err) => console.log(err));
+        
+     
+    // })
+    // .catch((err) => console.log(err));
+    // }
+
     return (
-        <div className="flex flex-col min-h-screen">
-        <Navbar className="w-full" />
-        <div className="flex">
-          <NavigateMyBooks  />
-          <div className="flex flex-col items-start justify-between w-full ml-12">
-            <h3 className="header-3 flex mt-10 justify-center text-center">{shelfName}</h3>
-            <div className="flex flex-col items-center h-64">
-              {books.length !== 0 ? books.map(el => <img key={el._id} src={el.cover} className="my-1 h-48" ></img>) : null}
-            </div>
-          </div>
-        </div>
-      </div>
+<div className="flex flex-col min-h-screen">
+  <Navbar className="w-full" />
+  <div className="flex">
+    <NavigateMyBooks className="mr-2 mt-6" />
+    <div className="flex flex-col items-center justify-center w-full">
+        <h3 className="header-3 flex mt-10 text-center mx-auto">{shelfName}</h3>
+        { books.length !== 0 ? <div className="grid grid-cols-6 gap-5 ">{books.map(el =><div className="relative">
+          <img key={el._id} src={el.cover} className="my-1 h-48" ></img>
+        {/* <button onClick={()=>handleClick(el._id)}>  
+        <i className="fa-solid fa-minus  icon-position text-black"></i></button> */}
+          </div>)}</div> : <p className="text-center mx-auto mt-10">There are no books on this shel yet</p> }
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
 
     )
 }
