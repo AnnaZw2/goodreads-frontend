9b5c3cb76f5197b473b4d121f353d6ddc00c31b1
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-01-26 00:16:47 +0100

    new imports and routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useContext, useEffect, useState,useMemo } from 'react';
+import React, { useContext, useEffect, useState, useMemo } from 'react';
 import { Route, Routes } from 'react-router';
 
 import './App.css'
@@ -18,36 +18,40 @@ import { userContext } from './context/userContex';
 import { updateShelfContext } from './context/updateShelfContext';
 import { User } from './pages/User';
 import { NotFound } from "./pages/NotFound"
-import {Details} from "./components/books/Details"
+import { Details } from "./pages/Details"
 import axios from "axios"
 import { searchShelfContext } from './context/searchContext';
+import { ShowUsers } from './pages/Admin/Show/ShowUsers';
+import { ShowComments } from './pages/Admin/Show/ShowComments';
+import { AdminForms } from './pages/Admin/AdminForms';
 
 function App() {
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("decoded")));
-const jwt = localStorage.getItem("jwt")
+  const jwt = localStorage.getItem("jwt")
 
-const [searchValue,setSearchValue] = useState("")
-const [searchOutput, setSearchOutput] = useState([]);
+  const [searchValue, setSearchValue] = useState("")
+  const [searchAdmin, setSearchAdmin] = useState("")
+  const [searchOutput, setSearchOutput] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const decode = JSON.parse(localStorage.getItem("decoded"));
- 
-setUser(decode)
-  },[jwt])
+
+    setUser(decode)
+  }, [jwt])
 
 
-useEffect(() => {
-  
-if(user!=null){
-  axios.get(`http://localhost:3000/users/${user.email}`,  { headers: { Authorization: `Bearer ${jwt}` }}).then(res => {setUser(res.data);}).catch(err => console.log(err))
+  useEffect(() => {
 
-}
-    
+    if (user != null) {
+      axios.get(`http://localhost:3000/users/${user.email}`, { headers: { Authorization: `Bearer ${jwt}` } }).then(res => { setUser(res.data); }).catch(err => console.log(err))
 
-      
+    }
 
-}, [jwt]);
+
+
+
+  }, [jwt]);
 
 
   const [updateShelves, setUpdateShelves] = useState(false)
@@ -57,30 +61,33 @@ if(user!=null){
   return (
     <div className="root bg-light-beige h-full " >
       <updateShelfContext.Provider value={{ updateShelves: updateShelves, setUpdateShelves: setUpdateShelves }} >
-      
-      <userContext.Provider value={{ user: user, setUpdateUser:setUser , jwt: jwt}}>
-      <searchShelfContext.Provider value={{searchValue:searchValue,setSearchValue:setSearchValue,searchOutput:searchOutput,setSearchOutput:setSearchOutput}}>
-          <Routes>
-        
-            <Route path="/" element={<Home />}></Route>
-            <Route path='/details/:id' element={<Details/>}></Route>
-            <Route path='/sign' element={<Register />}></Route>
-            <Route path='/login' element={<LogIn />}></Route>
-            <Route path='/explore' element={<Explore />}></Route>
-            <Route path='/settings' element={<Settings />}></Route>
-            <Route path="/notlogged" element={<NotLoggedIn />}></Route>
-            <Route path='/mybooks' element={<All/>}>    </Route>
-            <Route path="/users" element={<User />} />
-{ user.role === "admin" ?
-              <Route path="/admin" element={<Admin />}></Route> : null }
-            <Route path='mybooks/shelves/all' element={<All />}></Route>
-            <Route path='mybooks/stats' element={<Stats />}></Route>
-            <Route path="mybooks/shelves/:name/:id" element={<Shelf />}></Route>
-            <Route path='*' element={<NotFound />} />
 
-          </Routes>
+        <userContext.Provider value={{ user: user, setUpdateUser: setUser, jwt: jwt }}>
+          <searchShelfContext.Provider value={{ searchValue: searchValue, setSearchValue: setSearchValue, searchOutput: searchOutput, setSearchOutput: setSearchOutput, searchAdmin: searchAdmin, setSearchAdmin: setSearchAdmin }}>
+            <Routes>
+
+              <Route path="/" element={<Home />}></Route>
+              <Route path='/details/:id' element={<Details />}></Route>
+              <Route path='/sign' element={<Register />}></Route>
+              <Route path='/login' element={<LogIn />}></Route>
+              <Route path='/explore' element={<Explore />}></Route>
+              <Route path='/settings' element={<Settings />}></Route>
+              <Route path="/notlogged" element={<NotLoggedIn />}></Route>
+              <Route path='/mybooks' element={<All />}>    </Route>
+              <Route path="/users" element={<User />} />
+              {user.role === "admin" ?
+                <Route path="/admin" element={<Admin />}></Route> : null}
+              {user.role === "admin" ? <Route path='/admin/showusers' element={<ShowUsers />} /> : null}
+              {user.role === "admin" ? <Route path='/admin/showcomments' element={<ShowComments />} /> : null}
+              {user.role === "admin" ? <Route path='/admin/forms' element={<AdminForms/>} /> : null}
+              <Route path='mybooks/shelves/all' element={<All />}></Route>
+              <Route path='mybooks/stats' element={<Stats />}></Route>
+              <Route path="mybooks/shelves/:name/:id" element={<Shelf />}></Route>
+              <Route path='*' element={<NotFound />} />
+
+            </Routes>
           </searchShelfContext.Provider>
-          </userContext.Provider>
+        </userContext.Provider>
       </updateShelfContext.Provider>
     </div>
   )
